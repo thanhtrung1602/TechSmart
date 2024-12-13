046a65fb561da82113fd468d4c97b6c5fcbe945f
thanhtrung <thanhtrung160204@gmail.com>
2024-12-13 22:47:41 +0700

    update

--- server/src/models/attributeValue.js
+++ server/src/models/attributeValue.js
@@ -7,7 +7,13 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {}
+    static associate(models) {
+      AttributeValue.belongsTo(models.Attribute, {
+        foreignKey: "attributeId",
+        targetKey: "id",
+        as: "attributeData",
+      });
+    }
   }
   AttributeValue.init(
     {
